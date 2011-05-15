ee09761edacfcf286c20d024f3e6f01c41859bdd
ubuntu <ubuntu@ubuntu1.(none)>
2011-05-15 17:03:34 +0000

    Changes to date.

--- src/bristol_board/apps/bb_database/src/bb_database_server.erl
+++ src/bristol_board/apps/bb_database/src/bb_database_server.erl
@@ -23,10 +23,10 @@
 %% ------------------------------------------------------------------
 -export([start_link/0,
         is_api_key_present/1,
-        is_api_key_valid/1,
+        is_api_key_valid/1,        
         is_user_valid/2,
         
-        query_username_password/4]).
+        query_is_user_valid/4]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -59,7 +59,7 @@ init(_Args) ->
 
 handle_call({is_user_valid, Username, Password}, From, State) ->    
     bb_database_event:is_user_valid_call(Username, Password),   
-    proc_lib:spawn(?MODULE, query_username_password, [From, ?pgpool_auth_name, Username, Password]),
+    proc_lib:spawn(?MODULE, query_is_user_valid, [From, ?pgpool_auth_name, Username, Password]),
     {noreply, State};
     
 handle_call(_Request, _From, State) ->
@@ -78,39 +78,35 @@ code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 %% ------------------------------------------------------------------
-%% Internal Function Definitions
-%% ------------------------------------------------------------------
-query_username_password(From, Pool, Username, Password) ->
+%% Queries.
+%% ------------------------------------------------------------------    
+query_is_user_valid(From, Pool, Username, Password) ->
     Result = q(Pool, "SELECT true FROM articheck_user WHERE username = $1 AND password = crypt($2, password)", [Username, Password]),        
     case Result of
         [{true}] ->
             bb_database_event:is_user_valid_return(Username, Password, true),   
-            gen_server:reply(From, true);
+            gen_server:reply(From, {ok, true});
         _ ->
             bb_database_event:is_user_valid_return(Username, Password, false),   
-            gen_server:reply(From, false)
+            gen_server:reply(From, {ok, false})
     end.        
-   
+    
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------        
 q(P, Sql, Parameters) ->
-    case get_connection(P) of
-        {ok, C} ->
-            try
-                case pgsql:equery(C, Sql, Parameters) of
-                    {ok, _Cols, Rows} -> Rows;
-                    {ok, Rows} -> Rows
-                end
-            after
-                return_connection(P, C)
-            end
+    {ok, C} = get_connection(P),
+    try
+        case pgsql:equery(C, Sql, Parameters) of
+            {ok, _Cols, Rows} -> Rows;
+            {ok, Rows} -> Rows
+        end
+    after
+        return_connection(P, C)
     end.
     
 get_connection(P) ->
-    case pgsql_pool:get_connection(P, 1000) of
-        {ok, C} ->            
-            {ok, C};
-        Error -> 
-            Error
-    end.    
+    pgsql_pool:get_connection(P, 1000).
 
 return_connection(P, C) ->
     pgsql_pool:return_connection(P, C).
